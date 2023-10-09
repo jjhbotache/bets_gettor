fd0503c28e31235c35595bd40926df7f72690b67
jjhbotache <jjhuertasbotache@gmail.com>
2023-10-08 21:27:45 -0500

    with graphs

--- reactFrontSurebettor/src/const/consts.js
+++ reactFrontSurebettor/src/const/consts.js
@@ -1,5 +1,5 @@
-export const apiRoute = "http://127.0.0.1:5000";
-// export const apiRoute = "https://api-bettor.onrender.com";
+// export const apiRoute = "http://127.0.0.1:5000";
+export const apiRoute = "https://api-bettor.onrender.com";
 export const navLinks = [
   {
     name: "Home",
